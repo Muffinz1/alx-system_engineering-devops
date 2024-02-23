016838f02bf565a93e6b0191faf729aa771130fc
muffin <muffin@muffin.com>
2024-02-23 15:02:25 -0800

    flask and Werkzeug added

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
 # installing the flask package @ 2.1.0
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+# installing the Werkzeug 2.1.1 too
+
+package{'flask':
+ensure   =>'2.1.0',
+provider =>'pip3',
+}
+package{'Werkzeug':
+ensure   =>'2.1.1',
+provider =>'pip3',
 }
