662630741f57ba629a73b893b0b1cc8f523a0ddd
muffin <muffin@muffin.com>
2024-02-23 14:48:32 -0800

    ensured

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,4 @@
 # installing the flask package @ 2.1.0
-package { 'python3-pip':
-  ensure => installed,
-}
 exec { 'install-flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
-  require => Package['python3-pip'],
 }
