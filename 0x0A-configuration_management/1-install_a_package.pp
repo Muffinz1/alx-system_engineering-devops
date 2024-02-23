a1b0079ea6d8b34d0b169b4a6bd5966fbe0d0cac
muffinz2 <me@me.com>
2024-02-23 14:07:12 -0800

    version fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# a puppet file to install a package
-# ensure that the pip is installed 
-# form it install flask
-
-package{'python3-pip':
-	ensure => installed,
-}
-
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  require => Package['python3-pip'],
+# a puppet file to install flask @ version 2.1.0
+package{'flask':
+ensure   => '2.1.0',
+provider => 'pip',
 }
