e1dd59fd40694242d3a6995bb0716bb7dedaaf9f
muffin <muffin@muffin.com>
2024-03-04 11:26:15 -0800

    puppet header special response

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,11 @@
 #!/usr/bin/env puppet
 # creating a custom HTTP header response, but with Puppet
-package {'mginx':
-ensure   => installed,
+package { 'nginx':
+ensure => installed,
 }
 
-file_line {'header_response':
-ensure => 'present',
+file_line { 'header_response':
+ensure => present,
 path   => '/etc/nginx/sites-available/default',
-}
-
-exec {'add_x_header':
-command  => "sudo sed -i '19 i \\tadd_header X-Served-By ${hostname};' /etc/nginx/sites-available/default",
-provider => 'shell',
-}
-
-exec {'refresh':
-command  => 'sudo service nginx restart',
-provider => 'shell',
+line   => 'add_header X-Served-By $hostname;',
 }
