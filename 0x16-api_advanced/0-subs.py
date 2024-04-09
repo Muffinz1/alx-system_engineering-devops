1217d3c894c55df93ac746ab6f39b946752ab6b3
muffin <muffin@muffin.com>
2024-04-09 16:16:08 -0700

    subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,15 +9,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """gets the number of subs"""
-    reddit_api = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
+    reddit_api = ("https://api.reddit.com/r/{}/about.json".format(subreddit))
+    headers = {'User-Agent': 'My-User-Agent'}
     r = requests.get(reddit_api, headers=headers, allow_redirects=False)
 
-    if r.status_code != 200:
+    if r.status_code >= 300:
         return (0)
-    r = r.json()
-    if 'data' in r:
-        return (r.get('data').get('subscribers'))
-
-    else:
-        return (0)
+    return reddit_api.json().get("data").get("subscribers")
