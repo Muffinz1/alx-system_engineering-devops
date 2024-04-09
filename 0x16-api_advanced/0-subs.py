5a69b4a582594776e5e3c2d2a926481d97fe8e15
muffin <muffin@muffin.com>
2024-04-09 16:29:01 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
 Reddit api is used to get the number of subs
-for a given subreddit
 returns 0 if the subreddit is wrong
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """gets the number of subs"""
-    reddit_subs = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My Custom User Agent'}
-    r = requests.get(reddit_subs, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if r.status_code == 200:
-        subs = r.json()
-        return subs['data']['subscribers']
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
     else:
         return 0
