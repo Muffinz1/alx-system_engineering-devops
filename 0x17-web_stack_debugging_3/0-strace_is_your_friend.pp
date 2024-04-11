ad93cc87dd2128fcd9df62002030a67e13ab867b
muffin <muffin@muffin.com>
2024-04-11 15:28:25 -0700

    straced

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fixing the 500 error msg and automate it with puppet
-exec { 'fix-wordpress':
+exec { 'fixing-500-error':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
